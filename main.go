60015b09d47f46d898ffb99f7332b5c79f8a1346
Alnivel <252823221+Alnivel@users.noreply.github.com>
2026-01-19 21:24:20 +0200

    Add control via socket

--- main.go
+++ main.go
@@ -3,21 +3,64 @@ package main
 import (
 	"flag"
 
-	"github.com/BurntSushi/xgbutil/xevent"
 	"github.com/Alnivel/zentile/state"
+	"github.com/BurntSushi/xgbutil/xevent"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+	//TODO: Add separate parsing of args which applies on top of the config
 	setLogLevel()
+
+	daemon := true
+	if daemon {
+		runDaemon()
+	} else {
+		// sending command
+		runCli()
+	}
+
+}
+
+func runCli() {
+	socketChan := make(chan string)
+	_ = socketChan
+
+}
+
+func runDaemon() {
+
 	state.Populate()
 
 	t := initTracker(CreateWorkspaces())
 	actions := initActions(t)
 	bindKeys(actions)
 
-	// Run X event loop
-	xevent.Main(state.X)
+	//TODO: Implement opening and receiving from socket into channel
+	pingBefore, pingAfter, pingQuit := xevent.MainPing(state.X)
+	socketChan, err := ListenSocket()
+	if err != nil {
+		return
+	}
+
+	for {
+		select {
+		case <-pingBefore:
+			// Wait for the event to finish processing.
+			<-pingAfter
+		case socketCommand := <-socketChan:
+			action, exists := actions[socketCommand.name]
+			if exists {
+				action()
+				socketCommand.ReplyOk()
+			} else {
+				// TODO: Add command name to the reply
+				socketCommand.ReplyErr("Unknown command")
+			}
+		case <-pingQuit:
+			return
+		}
+	}
 }
 
 func setLogLevel() {
---
+++ socket.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+type SocketCommand struct {
+	name      string
+	replyChan chan<- string
+}
+
+func (command SocketCommand) ReplyOk() {
+	command.replyChan <- "OK"
+	close(command.replyChan)
+}
+
+func (command SocketCommand) ReplyErr(msg string) {
+	command.replyChan <- "ERR: " + msg
+	close(command.replyChan)
+}
+
+func ListenSocket() (<-chan SocketCommand, error) {
+	socketPath := "/tmp/echo.sock"
+
+	// Unlink the socket file before binding to prevent "address already in use" errors
+	if _, err := os.Stat(socketPath); err == nil {
+		if err := os.Remove(socketPath); err != nil {
+			return nil, err
+		}
+	}
+
+	// Listen on the Unix socket
+	listener, err := net.Listen("unix", socketPath)
+	if err == nil {
+		return nil, err
+	}
+	defer listener.Close()
+
+	// Handle graceful shutdown signals to ensure the socket file is removed
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, os.Interrupt, syscall.SIGTERM)
+	go func(c chan os.Signal) {
+		<-c
+		fmt.Println("Caught signal: shutting down and removing socket.")
+		listener.Close()
+		os.Exit(0)
+	}(sigc)
+
+	commandChan := make(chan SocketCommand)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Accept error: %v\n", err)
+			break
+		}
+		go handleConnection(conn, commandChan)
+	}
+	return commandChan, nil
+
+}
+
+func handleConnection(conn net.Conn, commandChan chan<- SocketCommand) {
+	defer conn.Close()
+
+	readBuf := make([]byte, 512)
+	commandBuf := make([]byte, 0, 512)
+ReadLoop:
+	for {
+		n, err := conn.Read(readBuf)
+		if err != nil {
+			// TODO: Add logger
+			return
+		}
+
+		for split := range bytes.SplitSeq(readBuf, []byte{0}) {
+			n -= len(split) + 1 // Account for the terminating null byte
+
+			if cap(commandBuf) < len(commandBuf)+len(split) {
+				err := sendReplyOutside(conn, "ERR: You talking too long")
+				if err != nil {
+					// TODO: Add logger
+					return
+				}
+			}
+
+			commandBuf = append(commandBuf, split...)
+
+			if n < 0 { // If there was no terminating null byte
+				// i.e. the command is split between reads
+				continue ReadLoop
+				// The current split is the last in the current readBuf
+				// So breaking inner or continuing outer loop is the same
+			}
+
+			commandName := string(commandBuf)
+			commandBuf = commandBuf[:0] // Reset the buffer
+
+			reply := sendCommandInside(commandChan, commandName)
+
+			err := sendReplyOutside(conn, reply)
+			if err != nil {
+				// TODO: Add logger
+				return
+			}
+
+		}
+	}
+}
+
+func sendCommandInside(commandChan chan<- SocketCommand, commandName string) (reply string) {
+	replyChan := make(chan string)
+	commandChan <- SocketCommand{name: commandName, replyChan: replyChan}
+	return <-replyChan
+}
+
+func sendReplyOutside(conn net.Conn, reply string) error {
+	terminatedReply := append([]byte(reply), 0)
+
+	_, err := conn.Write(terminatedReply)
+	return err
+}
